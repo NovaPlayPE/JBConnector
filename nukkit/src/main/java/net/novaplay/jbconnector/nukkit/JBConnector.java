f24f4a250448137477a89eaa3fef944e9e10365f
Ragnok123 <=>
2019-09-01 22:41:13 +0200

    Config, requires to add data

--- nukkit/src/main/java/net/novaplay/jbconnector/nukkit/JBConnector.java
+++ nukkit/src/main/java/net/novaplay/jbconnector/nukkit/JBConnector.java
@@ -1,10 +1,34 @@
 package net.novaplay.jbconnector.nukkit;
 
+import java.io.File;
+import java.io.IOException;
+
 import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.utils.Config;
 
 public class JBConnector extends PluginBase {
 	
+	private Config config = null;
+	
 	public void onEnable() {
+		setupConfig();
+	}
+	
+	public Config getPluginConfig() {
+		return this.config;
+	}
+	
+	private void setupConfig() {
+		File file = new File(getDataFolder() + "/config.yml");
+		if(!file.exists()) {
+			config = new Config(getDataFolder()+"/config.yml",Config.YAML);
+			config.save();
+		} else {
+			config = new Config(getDataFolder()+"/config.yml",Config.YAML);
+		}
+	}
+	
+	private void createConnection() {
 		
 	}
 
--- spigot/src/main/java/net/novaplay/jbconnector/spigot/JBConnector.java
+++ spigot/src/main/java/net/novaplay/jbconnector/spigot/JBConnector.java
@@ -2,9 +2,40 @@ package net.novaplay.jbconnector.spigot;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.configuration.Configuration;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.*;
+
 public class JBConnector extends JavaPlugin{
 	
+	private FileConfiguration config = null;
+	
 	public void onEnable() {
+		setupConfig();
+	}
+	
+	public FileConfiguration getPluginConfig() {
+		return config;
+	}
+	
+	private void setupConfig() {
+		File file = new File(getDataFolder(), "config.yml");
+		if(!file.exists()) {
+			file.getParentFile().mkdirs();
+			saveResource("config.yml",false);
+		}
+		config = new YamlConfiguration();
+		try {
+			config.load(file);
+		} catch(IOException | InvalidConfigurationException e) {
+			
+		}
+	}
+	
+	private void createConnection() {
 		
 	}
 
